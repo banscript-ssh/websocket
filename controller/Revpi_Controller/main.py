60748ab2eefe186d7f28c647dce97c2f9cb5f81b
banscript-ssh <syahrulsyaban2@gmail.com>
2026-05-01 21:25:27 +0700

    add main.py for testing not using hardware

--- controller/Revpi_Controller/main.py
+++ controller/Revpi_Controller/main.py
@@ -1,52 +1,14 @@
-# ============================== # IMPORT LIBRARY # ==============================
-import argparse
 import asyncio
-import logging
+import websockets
 
-from revpi.client_revpi import run_client
-from revpi.logging import init_db, init_csv
+async def test():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as ws:
+        await ws.send('{"join":"revpi"}')
+        print("Connected!")
 
-logger = logging.getLogger("main")
+        while True:
+            await ws.send('{"type":"heartbeat"}')
+            await asyncio.sleep(2)
 
-# ============================== # ASYNC MAIN RUNTIME # ==============================
-async def async_main(host: str, port: int, controller_id: str):
-    init_db()
-    init_csv()
-    logger.info("Controller logger initialized")
-
-    while True:
-        try:
-            await run_client(host, port, controller_id)
-
-        except Exception as e:
-            logger.exception("Controller disconnected: %s", e)
-
-        logger.warning("Reconnect in 3 seconds...")
-        await asyncio.sleep(3)
-
-# ============================== # MAIN ENTRY # ==============================
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--host", default="127.0.0.1")
-    parser.add_argument("--port", type=int, default=8765)
-    parser.add_argument("--id", default="C1")
-    parser.add_argument("--log", default="info")
-    args = parser.parse_args()
-
-    level = getattr(logging, args.log.upper(), logging.INFO)
-    logging.basicConfig(
-        level=level,
-        format="%(asctime)s %(levelname)s %(name)s: %(message)s"
-    )
-
-    try:
-        asyncio.run(
-            async_main(args.host, args.port, args.id)
-        )
-    except KeyboardInterrupt:
-        logger.info("Shutting down RevPi controller...")
-
-
-# ============================== # RUN PROGRAM # ==============================
-if __name__ == "__main__":
-    main()
+asyncio.run(test())
