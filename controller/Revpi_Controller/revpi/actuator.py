5d9479d0e65c068ffbc26a4d95cca6f3c48f5f0f
banscript-ssh <syahrulsyaban2@gmail.com>
2026-06-19 02:09:24 +0700

    Refactor Actuator

--- controller/Revpi_Controller/revpi/actuator.py
+++ controller/Revpi_Controller/revpi/actuator.py
@@ -6,28 +6,36 @@ def control_actuators(buttons):
     """
 
     state = {
+        "BUZZ1": 0,
+        "BUZZ2": 0,
+        "BUZZ3": 0,
         "LED1": 0,
         "LED2": 0,
         "LED3": 0,
         "LED4": 0,
-        "BUZZ1": 0,
+        "LED5": 0,
         "LED6": 0,
         "LED7": 0,
-        # "LED8": 0,
-        # "LED9": 0,
-        "BUZZ2": 0,
+        "LED8": 0,
+        "LED9": 0,
+        "LED10": 0,
+        "LED11": 0,
     }
 
     # PLC-style direct control (momentary)
-    state["LED1"] = 1 if buttons["PB1"] == 1 else 0
-    state["LED2"] = 1 if buttons["PB2"] == 1 else 0
-    state["LED3"] = 1 if buttons["PB3"] == 1 else 0
-    state["LED4"] = 1 if buttons["PB4"] == 1 else 0
-    state["BUZZ1"] = 1 if buttons["PB5"] == 1 else 0
-    state["LED6"] = 1 if buttons["PB6"] == 1 else 0
+    state["BUZZ1"] = 1 if buttons["PB1"] == 1 else 0
+    state["BUZZ2"] = 1 if buttons["PB2"] == 1 else 0
+    state["BUZZ3"] = 1 if buttons["PB3"] == 1 else 0
+    state["LED1"] = 1 if buttons["PB4"] == 1 else 0
+    state["LED2"] = 1 if buttons["PB5"] == 1 else 0
+    state["LED3"] = 1 if buttons["PB6"] == 1 else 0
+    state["LED4"] = 1 if buttons["PB7"] == 1 else 0
+    state["LED5"] = 1 if buttons["PB8"] == 1 else 0
+    state["LED6"] = 1 if buttons["PB9"] == 1 else 0
     state["LED7"] = 1 if buttons["PB7"] == 1 else 0
-    # state["LED8"] = 1 if buttons["PB8"] == 1 else 0
-    # state["LED9"] = 1 if buttons["PB9"] == 1 else 0
-    state["BUZZ2"] = 1 if buttons["PB8"] == 1 else 0
+    state["LED8"] = 1 if buttons["PB8"] == 1 else 0
+    state["LED9"] = 1 if buttons["PB8"] == 1 else 0
+    state["LED10"] = 1 if buttons["PB8"] == 1 else 0
+    state["LED11"] = 1 if buttons["PB8"] == 1 else 0
 
     return state
